c0f453df137073baa22381120948fbf786a1a3d4
Daeyeol Ryu <dy@yoobato.com>
2022-02-07 01:12:36 +0900

    prettier bug fix

--- src/theme.ts
+++ src/theme.ts
@@ -1,14 +1,14 @@
-import { DefaultTheme } from "styled-components";
+import { DefaultTheme } from 'styled-components';
 
 export const theme: DefaultTheme = {
-    red: "#E51013",
-    black: {
-        veryDark: "#141414",
-        darker: "#181818",
-        lighter: "#2F2F2F",
-    },
-    white: {
-        lighter: "#FFF",
-        darker: "#E5E5E5",
-    },
+  red: '#E51013',
+  black: {
+    veryDark: '#141414',
+    darker: '#181818',
+    lighter: '#2F2F2F',
+  },
+  white: {
+    lighter: '#FFF',
+    darker: '#E5E5E5',
+  },
 };
